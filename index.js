f45031311ac4e2de9f8c7d6d4fec6a964f7313c8
Andrew <snelling.andrew@gmail.com>
2021-04-15 13:58:33 +0200

    dotenv setup/loggedin to discord successfully

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+
+client.once('ready', () => {
+  console.log('ready!');
+});
+
+client.login(process.env.TOKEN);
